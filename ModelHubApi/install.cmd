a10baa8f011b37bfe07d147a3c70acdb0f5ea4cb
lianshufeng <251708339@qq.com>
2025-04-17 10:33:10 +0800

    update

---
+++ ModelHubApi/install.cmd
@@ -0,0 +1,14 @@
+
+echo :  update pip
+python -m pip install --upgrade pip
+
+
+echo :  install torch
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124
+
+echo :  updatetransformers
+pip uninstall transformers -y
+pip install --upgrade git+https://github.com/huggingface/transformers accelerate
+
+echo :  install fastapi
+pip install --upgrade fastapi uvicorn
