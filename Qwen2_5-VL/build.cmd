62f016524973797d4358aa9ee50a976ad018bb3e
lianshufeng <251708339@qq.com>
2025-04-28 16:36:17 +0800

    更新 dockerfile

--- Qwen2_5-VL/build.cmd
+++ Qwen2_5-VL/build.cmd
@@ -1,4 +1,5 @@
 set HTTP_PROXY=http://192.168.31.98:1080
 set HTTPS_PROXY=http://192.168.31.98:1080
-docker build ./ -f Dockerfile --build-arg HTTP_PROXY=http://192.168.31.98:1080 --build-arg HTTPS_PROXY=http://192.168.31.98:1080 -t lianshufeng/model_api:qwen2_5vl
+cd ../
+docker build ./ -f ./Qwen2_5-VL/Dockerfile --build-arg HTTP_PROXY=http://192.168.31.98:1080 --build-arg HTTPS_PROXY=http://192.168.31.98:1080 -t lianshufeng/model_api:qwen2_5vl
 pause
